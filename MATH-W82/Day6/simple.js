8189bc6fcd8a25eca8616a2e3bfd5f5e805b29d8
David-Dahms <55364390+David-Dahms@users.noreply.github.com>
2020-01-16 02:01:36 -0500

    7.3

--- MATH-W82/Day6/simple.js
+++ MATH-W82/Day6/simple.js
@@ -1,2 +1,19 @@
 
-svg.style('background-color', 'blue')
+d3.selectAll('svg#simple-pic')
+    // skyblue background
+    .style('background-color', 'skyblue')
+
+    // red border
+    .append('rect')
+    .attr('width', 600)
+    .attr('height', 200)
+    .style('stroke', 'red')
+    .style('strokewidth', 10)
+    .style('opacity', 1)
+    .style('fill', 'transparent')
+
+d3.select('h2')
+    // edits h2 text
+    .style('stroke', 'red')
+    .text('A Really Simple Description')
+    
